5e0302afb225464a07c3c679f906a96ed374de19
Wang Ningning <wangningning2coonnngggg@gmail.com>
2022-04-03 16:16:00 +0800

    some basic stuff about express.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.write('Hello');
-  res.end();
-});
+app.listen(port, () => {
+  console.log('starting service');
+})
 
-server.listen(3000, () => {
-  console.log('starting service')
+app.get('/', (req, res) => {
+  res.send('Hello, world')
+})
+
+const data = [
+  {
+    id: 1,
+    name: 'Andy'
+  },
+  {
+    id: 2,
+    name: 'Jack'
+  }
+]
+
+app.get('/posts', (req, res) => {
+  res.send(data)
+})
+
+app.get('/posts/:postId', (req, res) => {
+  // get params from header
+  const { postId } = req.params;
+
+  // filter
+  const posts = data.filter(item => item.id == postId);
+
+  // responsse
+  res.send(posts[0])
 })
