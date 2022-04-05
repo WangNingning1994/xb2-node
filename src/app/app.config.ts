33d8d4a6dc2ad3323483aa98e8ba9c6b94cef553
Wang Ningning <wangningning2coonnngggg@gmail.com>
2022-04-05 10:23:41 +0800

    Add env config file.

---
+++ src/app/app.config.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+/**
+ * config app
+ */
+
+export const { APP_PORT } = process.env;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import app from './app' // no need to specify index.js(ts)
+import { APP_PORT } from './app/app.config'
 
-app.listen(3000, () => {
+app.listen(APP_PORT, () => {
   console.log('starting service...');
 })
