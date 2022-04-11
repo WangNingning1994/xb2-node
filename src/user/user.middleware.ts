2d461d6a83ce7545c0adb12dc11087b42c2ad46e
Wang Ningning <wangningning2coonnngggg@gmail.com>
2022-04-11 21:01:03 +0800

    hash password.

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from './user.service';
+import bcrypt from 'bcrypt';
 
 /**
  * 验证用户数据
  */
-
 export const validateUserData = async (
   req: Request,
   res: Response,
@@ -22,6 +22,23 @@ export const validateUserData = async (
   // 验证用户名
   const user = await userService.getUserByName(name);
   if (user) return next(new Error('USER_ALREADY_EXIST'));
+
+  next();
+}
+
+/**
+ * HASH 密码
+ */
+export const hashPassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { password } = req.body;
+
+  // Hash password
+  req.body.password = await bcrypt.hash(password, 10);
 
   next();
 }
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { validateUserData } from './user.middleware'
+import { validateUserData, hashPassword } from './user.middleware'
 import * as userController from './user.controller'
 
 const router = express.Router();
@@ -7,7 +7,7 @@ const router = express.Router();
 /**
  * 创建用户
  */
-router.post('/users', validateUserData, userController.store);
+router.post('/users', validateUserData, hashPassword, userController.store);
 
 /**
  * 导出路由
